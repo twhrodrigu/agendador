32807dec1607f5b6b61f02622fbbd2fe8d0f0b29
Duda Dornelles <ddornell@thoughtworks.com>
2015-02-05 15:43:41 -0200

    Doing the heavy lifting on google api's - it is 'working' :)

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'net/https'
 
 
 module Calendar
@@ -9,10 +10,33 @@ module Calendar
   end
 
   def self.availability(api_token, params, duration_in_hours=1)
-    request = {
-      :timeMin => format(params[:start]),
-      :timeMax => format(params[:start], duration_in_hours),
-      :items => User.all(:office => 'Porto Alegre', :role => params[:role])
-    }
+    consultants = User.all(:office => 'Porto Alegre', :role => params[:role])
+    requests = []
+    while !consultants.empty? do
+      requests << {
+        :timeMin => format(params[:start]),
+        :timeMax => format(params[:start], duration_in_hours),
+        :items => consultants.shift(10)
+      }
+    end
+
+    responses = []
+    requests.each do |request|
+      uri = URI("https://www.googleapis.com/calendar/v3/freeBusy?key=#{ENV['G_API_KEY']}&alt=json")
+      req = Net::HTTP::Post.new(uri.path)
+      req.add_field('content-type', 'application/json')
+      req['Authorization'] = "Bearer #{api_token}"
+      req.body = JSON.dump(request)
+      responses << Net::HTTP.start(
+        uri.host, uri.port,
+        :use_ssl => uri.scheme == 'https',
+        :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+        https.request(req)
+      end
+    end
+    responses.map {|e| JSON.parse(e.body)["calendars"] }.
+      reduce(&:merge).
+      select { |k,v| v["busy"].empty? && v["errors"].nil? }.
+      map {|k,v| k }
   end
 end
