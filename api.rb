1209ef727dc96d11d37a049a66a06b521b8b02f0
thiagofelix <felixthi@gmail.com>
2015-02-26 19:36:31 -0300

    Add User and Roles as JSON API

--- api.rb
+++ api.rb
@@ -1,24 +1,44 @@
 require 'grape'
 
 module AgendaEntrevista
-    class API < Grape::API
-      version 'v1', using: :path
-      format :json
+  class API < Grape::API
+    version 'v1', using: :path
+    format :json
 
-      resource :calendar do
+    helpers do
+      def permitted_params
+          @permitted_params ||= declared(params, include_missing: false)
+      end
+    end
 
-        desc "Return people available for a given period"
-        params do
-          requires :token, type: String, desc: "Google Calendar Access Token"
-          requires :start, type: DateTime, desc: "Start datetime"
-          optional :hours, type: Integer, desc: "Hours available", default: 1
-          optional :role, type: String, desc: "Desired role", default: 'Dev'
-        end
-        get :available do
-          Calendar.availability(params[:token], params)
-        end
+    resource :calendar do
 
+      desc "Returns people available for a period"
+      params do
+        requires :token, type: String, desc: "Google Calendar Access Token"
+        requires :start, type: DateTime, desc: "The start of the interval for the query"
+        optional :hours, type: Integer, desc: "The number of hours should be available", default: 1
+        optional :role, type: String, desc: "The role of interest", default: 'Dev'
+      end
+      get :available do
+        Calendar.availability(params[:token], permitted_params)
       end
 
     end
+
+    desc "Return list of roles available"
+    get :roles do
+      User.roles
+    end
+
+    desc "Return consultants based on the office and roles"
+    params do
+      requires :office, type: String, desc: "The office where they work"
+      requires :role, type: String, desc: "The role they belong"
+    end
+    get :consultants do
+      User.all(permitted_params)
+    end
+
+  end
 end
