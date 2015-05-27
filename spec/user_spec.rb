8ec7e4a53e48e8916b579c634e2fb996ad639d14
Marcelo Vargas <marcelo.vargas90@gmail.com>
2015-05-27 17:53:21 -0300

    added test for get consultants from jigsaw api.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -27,4 +27,25 @@ describe "User tests" do
     expect(last_response.body).to eq(['Porto Alegre','São Paulo'].to_json)
   end
 
+  it 'return consultants' do
+    json = [
+      {
+        "role" => {"name"=>"Dev"},
+        "loginName" => "aconsultant"
+      },
+      {
+        "role" => {"name"=>"Dev"},
+        "loginName" => "bconsultant"
+      }
+    ].to_json
+
+    WebMock.stub_request(:get, "https://jigsaw.thoughtworks.com/api/people?page=1&staffing_office=Porto%20Alegre").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'e9c735405f523e81f2f2884ea116411f', 'Content-Type'=>'application/json', 'Host'=>'jigsaw.thoughtworks.com', 'User-Agent'=>'Ruby', 'X-Service-Version'=>'2'}).
+         to_return(:status => 200, :body => json, :headers => {})
+
+    get '/v1/consultants', params = {:office => "Porto Alegre", :role => "Dev"}
+
+    expect(last_response.body).to eq([{:id => "aconsultant@thoughtworks.com"},{:id => "bconsultant@thoughtworks.com"}].to_json)
+  end
+
 end
