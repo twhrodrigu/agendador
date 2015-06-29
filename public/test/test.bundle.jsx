c5e88b988746472618cbd6c1140f6043de712ed5
Francieli Viane <francielifrv@gmail.com>
2015-06-29 13:13:25 -0300

    remove unused files and directories for javascript tests
    @FFrancieli/ @pedroaugustofr

--- public/test/test.bundle.jsx
+++
@@ -1,6 +0,0 @@
-/** @jsx React.DOM */
-var context = require.context('./spec', true, /.jsx$/);
-
-//make sure you have your directory and regex test set correctly!
-context.keys().forEach(context);
-module.exports = context;
