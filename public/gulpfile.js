832436c91a9cb7bf73bf9eeaf5e1b0756013d447
pprado <pprado@thoughtworks.com>
2015-07-02 11:49:42 -0300

    Create watch files with gulp to build js, jsx and less using webpack - @ppradotw @pedroaugustofr

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -2,53 +2,21 @@ var gulp = require('gulp');
 var react = require('gulp-react');
 var inject = require('gulp-inject');
 var bowerFiles = require('main-bower-files');
-
-var paths = {
-    js: ['src/**/*.js'],
-    jsx: ['src/**/*.jsx'],
-    jsDist: ['dist/**/*.js']
-};
-
-var compileJsx = function(path) {
-    var destPath;
-
-    if(typeof path == "string")  {
-        destPath = 'dist/' + path.split("/src/")[1];
-        destPath = destPath.split(/\/[A-Za-z0-9]{1,}.jsx/)[0];
-    }
-    else {
-        destPath = 'dist';
-    }
-
-    gulp.src(path)
-        .pipe(react())
-        .pipe(gulp.dest(destPath));  
-}
-
-gulp.task('copyJs', function() {
-    gulp.src(paths.js)
-        .pipe(gulp.dest('dist'));
-})
-
-gulp.task('index', function () {
-    var bowerSources = gulp.src(bowerFiles(), {read: false});
-    var sources = gulp.src(paths.jsDist, {read: false});
- 
-    gulp.src('./template/index.html')
-        .pipe(inject(bowerSources, {name: 'bower'}))
-        .pipe(inject(sources))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('build', function() {
-    compileJsx(paths.jsx);
-    gulp.start('copyJs', 'index');
-});
+var run = require('gulp-run');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
 
 gulp.task('watch', function() {
-    gulp.watch(paths.jsx).on('change', function(file) {
-        compileJsx(file.path);
+    gulp.watch(['src/**/*.jsx', 'src/**/*.js', 'less/**/*.less']).on('change', function(file) {
+        gulp.start('webpack');
     });
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('webpack', function() {
+    webpack(webpackConfig, function(error, status) {
+
+    });
+});
+
+
+gulp.task('default', ['webpack', 'watch']);
