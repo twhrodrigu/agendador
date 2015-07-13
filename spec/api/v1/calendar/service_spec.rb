6ad6f8c05c8432bffd3493450f714b1c60eaccd0
Felipe Dornelas <felipead@gmail.com>
2015-07-13 18:00:20 -0300

    Improving calendar tests @felipead, @patricktprado

--- spec/api/v1/calendar/service_spec.rb
+++ spec/api/v1/calendar/service_spec.rb
@@ -6,27 +6,36 @@ module API
   module V1
     describe 'calendar service' do
 
+      before(:all) do
+        @token = 'access-token-123'
+      end
+
       it 'format period with timezone' do
         format = Calendar::Service.format('2015-02-25T12:00:00', 1)
         expect(format).to eq('2015-02-25T13:00:00+00:00')
       end
 
-      it 'return people available from google calendar' do
-        freeBusyJson = JSON.dump(
+      it 'should return only available consultants' do
+        alice = Consultants::Consultant.new login: 'alice', name: 'Alice', role: 'QA'
+        bob = Consultants::Consultant.new login: 'bob', name: 'Bob', role: 'Dev'
+        eve = Consultants::Consultant.new login: 'eve', name: 'Eve', role: 'BA'
+
+        calendar_response = JSON.dump(
           {
             'calendars' => {
-              'alice@thoughtworks.com' => {'busy' => []}
+              alice.email => {'busy' => [{'start'=>'2015-02-25T12:00:00Z', 'end'=>'2015-02-25T14:00:00Z'}]},
+              bob.email   => {'busy' => []},
+              eve.email   => {'busy' => [{'start'=>'2015-02-25T10:00:00Z', 'end'=>'2015-02-25T12:30:00Z'}]}
             }
           }
         )
 
-        alice = Consultants::Consultant.new(login: 'alice', name: 'Alice', role: 'QA')
+        WebMock.stub_request(:post, 'https://www.googleapis.com/calendar/v3/freeBusy').to_return({:body => calendar_response, :status => 200})
 
-        WebMock.stub_request(:post, 'https://www.googleapis.com/calendar/v3/freeBusy').to_return({:body => freeBusyJson, :status => 200})
-
-        expect(Calendar::Service.availability token: 'access-token-123', start: '2015-02-25T12:00:00', hours: 1, consultants: [alice]).to eq([alice])
+        available = Calendar::Service.availability token: @token, start: '2015-02-25T12:00:00', hours: 1, consultants: [alice, bob, eve]
+        expect(available).to eq([bob])
       end
-
+      
     end
   end
 end
