f50e802ab32ae43ba62558d33c15f27047338506
Felipe Dornelas <felipead@gmail.com>
2015-07-14 17:27:54 -0300

    Test for no consultants available at calendar service @felipead @patricktprado

--- spec/api/v1/calendar/service_spec.rb
+++ spec/api/v1/calendar/service_spec.rb
@@ -8,6 +8,9 @@ module API
 
       before(:all) do
         @token = 'access-token-123'
+        @alice = Consultants::Consultant.new login: 'alice', name: 'Alice', role: 'QA'
+        @bob = Consultants::Consultant.new login: 'bob', name: 'Bob', role: 'Dev'
+        @eve = Consultants::Consultant.new login: 'eve', name: 'Eve', role: 'BA'
       end
 
       it 'format period with timezone' do
@@ -16,26 +19,37 @@ module API
       end
 
       it 'should return only available consultants' do
-        alice = Consultants::Consultant.new login: 'alice', name: 'Alice', role: 'QA'
-        bob = Consultants::Consultant.new login: 'bob', name: 'Bob', role: 'Dev'
-        eve = Consultants::Consultant.new login: 'eve', name: 'Eve', role: 'BA'
-
         calendar_response = JSON.dump(
           {
             'calendars' => {
-              alice.email => {'busy' => [{'start'=>'2015-02-25T12:00:00Z', 'end'=>'2015-02-25T14:00:00Z'}]},
-              bob.email   => {'busy' => []},
-              eve.email   => {'busy' => [{'start'=>'2015-02-25T10:00:00Z', 'end'=>'2015-02-25T12:30:00Z'}]}
+              @alice.email => {'busy' => [{'start'=>'2015-02-25T12:00:00Z', 'end'=>'2015-02-25T14:00:00Z'}]},
+              @bob.email   => {'busy' => []},
+              @eve.email   => {'busy' => [{'start'=>'2015-02-25T10:00:00Z', 'end'=>'2015-02-25T12:30:00Z'}]}
             }
           }
         )
 
         WebMock.stub_request(:post, 'https://www.googleapis.com/calendar/v3/freeBusy').to_return({:body => calendar_response, :status => 200})
 
-        available = Calendar::Service.availability token: @token, start: '2015-02-25T12:00:00', hours: 1, consultants: [alice, bob, eve]
-        expect(available).to eq([bob])
+        available = Calendar::Service.availability token: @token, start: '2015-02-25T12:00:00', hours: 1, consultants: [@alice, @bob, @eve]
+        expect(available).to eq([@bob])
       end
       
+      it 'should return empty list if no consultants are available' do
+        calendar_response = JSON.dump(
+          {
+            'calendars' => {
+              @alice.email => {'busy' => [{'start'=>'2015-02-25T12:00:00Z', 'end'=>'2015-02-25T14:00:00Z'}]},
+              @bob.email   => {'busy' => [{'start'=>'2015-02-25T12:00:00Z', 'end'=>'2015-02-25T14:00:00Z'}]},
+              @eve.email   => {'busy' => [{'start'=>'2015-02-25T10:00:00Z', 'end'=>'2015-02-25T12:30:00Z'}]}
+            }
+          }
+        )
+        WebMock.stub_request(:post, 'https://www.googleapis.com/calendar/v3/freeBusy').to_return({:body => calendar_response, :status => 200})
+
+        available = Calendar::Service.availability token: @token, start: '2015-02-25T12:00:00', hours: 1, consultants: [@alice, @bob, @eve]
+        expect(available).to eq([])
+      end
     end
   end
 end
