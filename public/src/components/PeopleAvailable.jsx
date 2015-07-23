ae3a09647f01c6189f39412ad0f886c1644e9bdb
Pedro Rocha <pedro.ownlux@gmail.com>
2015-07-23 20:08:53 -0300

    Remove roles and offices requets
    @pedroaugustofr

--- public/src/components/PeopleAvailable.jsx
+++ public/src/components/PeopleAvailable.jsx
@@ -20,24 +20,6 @@ var React = require('react'),
 var PeopleAvailable = React.createClass({
   mixins: [ Authentication ],
 
-  componentDidMount: function() {
-    request
-      .get('/v1/roles')
-      .end(function (e, r) {
-        this.setState({
-          roles: r.body.map((role, idx) => ({payload: idx, text: role}))
-        });
-      }.bind(this));
-
-      request
-        .get('/v1/offices')
-        .end(function (e, r) {
-          this.setState({
-            offices: r.body.map((office, idx) => ({payload: idx, text: office}))
-          });
-        }.bind(this));
-  },
-
   getInitialState: function() {
     return {
       selectedDate: DateInput.now(),
@@ -45,9 +27,9 @@ var PeopleAvailable = React.createClass({
       selectedEndTime: null,
       selectedRoleIndex: 0,
       selectedOfficeIndex: 0,
-      roles: [],
+      roles: ["BA", "Dev", "QA", "UI Dev", "XD", "PM", "DevOps"],
       people: [],
-      offices:[]
+      offices:["São Paulo", "Porto Alegre", "Recife", "Belo Horizonte"]
     }
   },
 
